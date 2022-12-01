329c440f76f5c9d3f7c6354c4e1fcc43611125c3
Dima Kryshkovec <nunam@internet.ru>
2022-12-01 22:35:26 +0300

    complete

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,22 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [msg, setMsg] = useState(-1);
+    const callback = (id: number) => {
+        setMsg(id);
+    };
+
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        return () => {
+            setMsg(-1);
+            unsubscribe(props.sourceId, callback);
+        };
+    }, [props.sourceId]);
+    return (
+        <div>
+            {props.sourceId}: {msg}
+        </div>
+    );
 }
